e3bcdbf0612fa577f169a5ed55356c0e80fa2a06
Szymon Domżalski <szymon.domzalski@fujitsu.com>
2025-09-03 15:17:10 +0000

    Artifact generation adjusted

--- ci_tools/generate_nodedriver_yaml.sh
+++ ci_tools/generate_nodedriver_yaml.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i 's/<sha256_checksum>/'$(sha256sum docker-machine-driver-fsas | cut -d" " -f 1)'/g' ./ci_tools/fsas_nodedriver_template.yml
 sed -i 's#<fsas_nodedriver_binary_url>#'https://github.com/fujitsu/docker-machine-driver-fsas/releases/download/\<placeholder\>/docker-machine-driver-fsas'#g' ./ci_tools/fsas_nodedriver_template.yml
-mv ./ci_tools/fsas_nodedriver_template.yml ./ci_tools/docker-machine-driver-fsas.yml
+mv ./ci_tools/fsas_nodedriver_template.yml ./docker-machine-driver-fsas.yml
