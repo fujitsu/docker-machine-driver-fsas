a365c754c7d625c38e86161a20375c20acf28028
lukasz-piotrowski-fujitsu <lukasz.piotrowski@fujitsu.com>
2025-10-03 12:54:58 +0200

    new workflows for testing, creating release and generating changelog … (#6)
    * new workflows for testing, creating release and generating changelog file; pass tag to bash script that generates yaml config file;
    
    * restore building binary in setup workflow; remove generating file with sha256 because github deliver it out of the box;

--- ci_tools/generate_nodedriver_yaml.sh
+++ ci_tools/generate_nodedriver_yaml.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+if [[ ! $1 ]]; then
+  echo "Pass tag as param e.g. $0 v1.2.3"
+  exit 2
+fi
+
+tag=$1
+url="https://github.com/fujitsu/docker-machine-driver-fsas/releases/download/$tag/docker-machine-driver-fsas"
+
 sed -i 's/<sha256_checksum>/'$(sha256sum docker-machine-driver-fsas | cut -d" " -f 1)'/g' ./ci_tools/fsas_nodedriver_template.yml
-sed -i 's#<fsas_nodedriver_binary_url>#'https://github.com/fujitsu/docker-machine-driver-fsas/releases/download/\<placeholder\>/docker-machine-driver-fsas'#g' ./ci_tools/fsas_nodedriver_template.yml
+sed -i 's#<fsas_nodedriver_binary_url>#'$url'#g' ./ci_tools/fsas_nodedriver_template.yml
 mv ./ci_tools/fsas_nodedriver_template.yml ./docker-machine-driver-fsas.yml
