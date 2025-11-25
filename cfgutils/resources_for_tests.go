c85f7cbaa09d74f9e09a5283abcb7494e84e7fe7
Lukasz Piotrowski <lukasz.piotrowski@fujitsu.com>
2025-11-25 13:29:59 +0100

    parametrize resetOsMocks function; add unit tests for section write_files;

--- cfgutils/resources_for_tests.go
+++ cfgutils/resources_for_tests.go
@@ -8,14 +8,14 @@ runcmd:
   - timedatectl set-timezone Europe/Warsaw
 `
 
-	userdataSampleContentNoSectionRunCmd = `#cloud-config`
-	userdataSampleInvalidYamlContent     = `.32??#(&&)58ffo:bar`
+	userdataSampleContentNoSections  = `#cloud-config`
+	userdataSampleInvalidYamlContent = `.32??#(&&)58ffo:bar`
 
 	inputOneItemRunCmd = []string{
 		`echo "Boot completed at $(date)" >> /tmp/cloud-config-test-runcmd.log`,
 	}
 
-	expectedStr2Cmd1Write = `
+	expectedStr2Cmd = `
 #cloud-config
 runcmd:
   - timedatectl set-timezone Europe/Warsaw
@@ -27,7 +27,7 @@ runcmd:
 		`echo "Cloud config test succeeded" >> /tmp/cloud-config-test-runcmd.log`,
 	}
 
-	expectedStr3Cmd1Write = `
+	expectedStr3Cmd = `
 #cloud-config
 runcmd:
   - timedatectl set-timezone Europe/Warsaw
@@ -36,11 +36,59 @@ runcmd:
 `
 	expectedErrorReadingFromFile = errors.New("error while reading file")
 
-	expectedStr1Cmd1Write = `
+	expectedStr1Cmd = `
 #cloud-config
 runcmd:
   - echo "Boot completed at $(date)" >> /tmp/cloud-config-test-runcmd.log
 `
 
 	expectedErrorWritingToFile = errors.New("error while writing file")
+
+	userdataSampleContentWriteFiles = `#cloud-config
+write_files:
+  - path: /tmp/foo
+    content: Foo was here
+    encoding: "gzip+b64"
+    permissions: "0644"`
+
+	inputOneItemWriteFiles = []CloudConfigItem{
+		NewCloudConfigItemWriteFiles("/tmp/cloud-config-test-write-files.log", "Cloud config succeeded for write_files")}
+
+	inputTwoItemsWriteFiles = []CloudConfigItem{
+		NewCloudConfigItemWriteFiles("/tmp/cloud-config-test-write-files.log", "Cloud config succeeded for write_files"),
+		NewCloudConfigItemWriteFiles("/tmp/cloud-config-test-write-files-2.log", "Cloud config succeeded for write_files part 2"),
+	}
+
+	expectedStr1Write = `#cloud-config
+write_files:
+  - path: /tmp/cloud-config-test-write-files.log
+    content: H4sIAAAAAAAA/wAAAP//cs7JL01RSM7PS8tMVyguTU5OTU1JTVFIyy9SKC/KLEmNT8vMSS0GBAAA//84FqCbJgAAAA==
+    encoding: "gzip+b64"
+    permissions: "0644"`
+
+	expectedStr2Write = `#cloud-config
+write_files:
+  - path: /tmp/foo
+    content: Foo was here
+    encoding: "gzip+b64"
+    permissions: "0644"
+  - path: /tmp/cloud-config-test-write-files.log
+    content: H4sIAAAAAAAA/wAAAP//cs7JL01RSM7PS8tMVyguTU5OTU1JTVFIyy9SKC/KLEmNT8vMSS0GBAAA//84FqCbJgAAAA==
+    encoding: "gzip+b64"
+    permissions: "0644"`
+
+	expectedStr3Write = `#cloud-config
+write_files:
+  - path: /tmp/foo
+    content: Foo was here
+    encoding: "gzip+b64"
+    permissions: "0644"
+  - path: /tmp/cloud-config-test-write-files.log
+    content: H4sIAAAAAAAA/wAAAP//cs7JL01RSM7PS8tMVyguTU5OTU1JTVFIyy9SKC/KLEmNT8vMSS0GBAAA//84FqCbJgAAAA==
+    encoding: "gzip+b64"
+    permissions: "0644"
+  - path: /tmp/cloud-config-test-write-files-2.log
+    content: H4sIAAAAAAAA/wAAAP//cs7JL01RSM7PS8tMVyguTU5OTU1JTVFIyy9SKC/KLEmNT8vMSS1WKEgsKlEwAgQAAP//55tZZi0AAAA=
+    encoding: "gzip+b64"
+    permissions: "0644"`
 )
