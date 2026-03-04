72da69698042b1978932ccff70d8512d1e0833c6
lukasz-piotrowski-fujitsu <lukasz.piotrowski@fujitsu.com>
2026-03-04 14:38:30 +0100

    Task/3140 (#24)
    * implement new feature: extend userdata file;
    
    * dirty commit; working unit tests
    
    * remove assessment section for testing userdata extending
    
    * add test for write error; change variable names;
    
    * remove section write_files when testing section runcmd;
    
    * add unit test for yaml unmarshaling; replace ifs with asserts;
    
    * parametrize resetOsMocks function; add unit tests for section write_files;
    
    * new unit tests for private method extendUserdata;
    
    * check unmarshaling error for all methods;
    
    * rename test resources file to follow naming convention; add unit tests for unmarshaling error; change interface method names according to review suggestions; add condition for checking if unmarshaled data has right structure - list of interfaces (panic prevention);
    
    * implement new feature: extend userdata file;
    
    * implement new feature: extend userdata file;
    
    * add new feature: set permissions to file in section write_files; add unit test;
    
    * new function that set custom permissions; add unit test;
    
    * use dependency injection to simplify unit test; remove global vars;
    
    * Rollback evidence;
    
    * rebase to feature/cdi-1175
    
    * removed not used function;
    
    * use temporary file instead of dependency injection;
    
    * corrections after review: formatting logs, change interface{} to any, rename struct,

--- cfgutils/cfgutils_test.go
+++ cfgutils/cfgutils_test.go
@@ -3,10 +3,14 @@ package cfgutils
 import (
 	"encoding/json"
 	"fmt"
+	"io/fs"
+	"os"
 	"testing"
 
 	"github.com/fujitsu/docker-machine-driver-fsas/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/yaml.v3"
 )
 
 func TestIsInit_Fail(t *testing.T) {
@@ -16,7 +20,7 @@ func TestIsInit_Fail(t *testing.T) {
 }
 
 func TestIsInit_Success(t *testing.T) {
-	manager := NewStandardCfgManager("[]")
+	manager := NewStandardCfgManager("[]", "")
 	observed := manager.IsInit()
 	assert.Equal(t, true, observed)
 }
@@ -44,7 +48,7 @@ hostname: `,
 
 	for _, tc := range testCases {
 		t.Run(tc.hostname, func(t *testing.T) {
-			manager := NewStandardCfgManager("[]")
+			manager := NewStandardCfgManager("[]", "")
 			observed := manager.PrepareMetadata(tc.instanceId, tc.hostname)
 			assert.Equal(t, tc.expected, observed)
 		})
@@ -62,7 +66,7 @@ func Test_prepareRke2ConfigProviderId(t *testing.T) {
 			expected: `kubelet-arg+: "provider-id=fsas-cdi://"`},
 	}
 
-	manager := NewStandardCfgManager("[]")
+	manager := NewStandardCfgManager("[]", "")
 
 	for _, tc := range testCases {
 		t.Run(tc.machineUUID, func(t *testing.T) {
@@ -80,7 +84,9 @@ func Test_prepareRke2ConfigNodeLabelsForGpu(t *testing.T) {
 		{name: "no GPU resources",
 			expected: ""},
 	}
-	manager := NewStandardCfgManager("[]")
+
+	manager := NewStandardCfgManager("[]", "")
+
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			observed := manager.prepareRke2ConfigNodeLabelsForGpu()
@@ -113,7 +119,9 @@ func Test_prepareRke2ConfigNodeLabels_Dynamic(t *testing.T) {
 			"max_resource_count": 3
 		}
 	]`
-	manager := NewStandardCfgManager(devicesSpecJson)
+
+	manager := NewStandardCfgManager(devicesSpecJson, "")
+
 	labelStr := manager.prepareRke2ConfigNodeLabelsForGpu()
 	expected := `kubelet-arg+: "node-labels=cohdi.io/nvidia-l40s-size-min=2,cohdi.io/nvidia-l40s-size-max=3"`
 	assert.Equal(t, expected, labelStr)
@@ -127,16 +135,19 @@ func TestPrepareRke2ConfigScript(t *testing.T) {
 	}{
 		{machineUUID: "cdd792f2-5591-4c18-a8bd-1c39e55dedfa",
 			expected: fmt.Sprintf(rke2ConfigScriptContent, configName,
-				`kubelet-arg+: "provider-id=fsas-cdi://cdd792f2-5591-4c18-a8bd-1c39e55dedfa"`)},
+				`kubelet-arg+: "provider-id=fsas-cdi://cdd792f2-5591-4c18-a8bd-1c39e55dedfa"`),
+		},
 		{machineUUID: "1234",
 			expected: fmt.Sprintf(rke2ConfigScriptContent, configName,
-				`kubelet-arg+: "provider-id=fsas-cdi://1234"`)},
+				`kubelet-arg+: "provider-id=fsas-cdi://1234"`),
+		},
 		{machineUUID: "",
 			expected: fmt.Sprintf(rke2ConfigScriptContent, configName,
-				`kubelet-arg+: "provider-id=fsas-cdi://"`)},
+				`kubelet-arg+: "provider-id=fsas-cdi://"`),
+		},
 	}
 
-	manager := NewStandardCfgManager("[]")
+	manager := NewStandardCfgManager("[]", "")
 
 	for _, tc := range testCases {
 		t.Run(tc.machineUUID, func(t *testing.T) {
@@ -160,21 +171,487 @@ func TestPrepareRke2ConfigScript_WithGPUResources(t *testing.T) {
 			"max_resource_count": 2
 		}
 	]`
-	manager := NewStandardCfgManager(devicesSpecJson)
+	manager := NewStandardCfgManager(devicesSpecJson, "")
+
 	configName := "100-gpu-labels"
 	script := manager.PrepareRke2ConfigScript(configName, "my-machine-uuid")
+
 	expected := fmt.Sprintf(rke2ConfigScriptContent, configName,
 		`kubelet-arg+: "provider-id=fsas-cdi://my-machine-uuid"
 kubelet-arg+: "node-labels=cohdi.io/nvidia-l40s-size-min=1,cohdi.io/nvidia-l40s-size-max=2"`)
 	assert.Equal(t, expected, script)
 }
+
 func Test_prepareRke2ConfigNodeLabels_FromExactJSON(t *testing.T) {
 	devicesSpecJson := `testJson`
+
 	var resources []models.Resource
 	if err := json.Unmarshal([]byte(devicesSpecJson), &resources); err != nil {
 		t.Logf("Failed to unmarshal JSON: %v", err)
 	}
-	manager := NewStandardCfgManager(devicesSpecJson)
+
+	manager := NewStandardCfgManager(devicesSpecJson, "")
+
 	labels := manager.prepareRke2ConfigNodeLabelsForGpu()
 	t.Logf("Generated GPU label: %s", labels)
+}
+
+func TestExtendUserdata(t *testing.T) {
+	testCases := []struct {
+		name              string
+		readFileContent   []byte
+		input             []CloudConfigItem
+		expectedStr       string
+		nrExpectedItemsWF int
+		nrExpectedItemsRC int
+		expectedError     error
+	}{
+		{name: "case 1: add 1 item to section 'runcmd'",
+			readFileContent:   []byte(userdataSampleContentBothSections),
+			input:             input1ItemRunCmdCast,
+			expectedStr:       expectedStr2Cmd1Write,
+			nrExpectedItemsRC: 2,
+			nrExpectedItemsWF: 1,
+			expectedError:     nil,
+		},
+
+		{name: "case 2: add 1 item to section 'runcmd' and 1 item to 'write_files'",
+			readFileContent:   []byte(userdataSampleContentBothSections),
+			input:             input1ItemRunCmdCast1ItemWriteFiles,
+			expectedStr:       expectedStr2Cmd2Write,
+			nrExpectedItemsRC: 2,
+			nrExpectedItemsWF: 2,
+			expectedError:     nil,
+		},
+
+		{name: "case 3: add 2 items to section 'runcmd' and 2 items to 'write_files'",
+			readFileContent:   []byte(userdataSampleContentBothSections),
+			input:             input2ItemsRunCmdCast2ItemsWriteFiles,
+			expectedStr:       expectedStr3Cmd3Write,
+			nrExpectedItemsRC: 3,
+			nrExpectedItemsWF: 3,
+			expectedError:     nil,
+		},
+
+		{name: "case 4: no section 'runcmd' available section 'write_files' 1 item cmd, 1 item write",
+			readFileContent:   []byte(userdataSampleContentCmdNoWriteYes),
+			input:             input1ItemRunCmdCast1ItemWriteFiles,
+			expectedStr:       expectedStr1Cmd2Write,
+			nrExpectedItemsRC: 1,
+			nrExpectedItemsWF: 2,
+			expectedError:     nil,
+		},
+
+		{name: "case 5: no section 'write_files' available section 'runcmd' 1 item cmd, 1 item write",
+			readFileContent:   []byte(userdataSampleContentCmdYesWriteNo),
+			input:             input1ItemRunCmdCast1ItemWriteFiles,
+			expectedStr:       expectedStr2Cmd1WriteBis,
+			nrExpectedItemsRC: 2,
+			nrExpectedItemsWF: 1,
+			expectedError:     nil,
+		},
+
+		{name: "case 6: no section 'write_files' neither 'runcmd' 1 item cmd, 1 item write",
+			readFileContent:   []byte(userdataSampleContentNoSections),
+			input:             input1ItemRunCmdCast1ItemWriteFiles,
+			expectedStr:       expectedStr1Cmd1Write,
+			nrExpectedItemsRC: 1,
+			nrExpectedItemsWF: 1,
+			expectedError:     nil,
+		},
+
+		{name: "case 7: input as empty list",
+			readFileContent:   []byte(userdataSampleContentBothSections),
+			input:             []CloudConfigItem{},
+			expectedStr:       userdataSampleContentBothSections,
+			nrExpectedItemsWF: 1,
+			nrExpectedItemsRC: 1,
+			expectedError:     nil,
+		},
+	}
+
+	var expected, observed map[string][]any
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+
+			tempFile, err := os.CreateTemp(t.TempDir(), "userdata.yaml")
+			require.NoError(t, err, "Failed to create temp file")
+			defer func() {
+				err := tempFile.Close()
+				require.NoError(t, err, "Failed to close temp file")
+				err = os.Remove(tempFile.Name())
+				require.NoError(t, err, "Failed to delete temp file")
+			}()
+
+			if _, err := tempFile.WriteString(string(tc.readFileContent)); err != nil {
+				require.NoError(t, err, "Failed to write to temp file")
+			}
+
+			sc := NewStandardCfgManager("[]", tempFile.Name())
+			err = sc.extendUserdata(tc.input)
+
+			if tc.expectedError != nil {
+				assert.ErrorIs(t, err, tc.expectedError,
+					fmt.Sprintf("expected: %v, but got: %v", tc.expectedError, err))
+			} else {
+
+				/* convert to YAML objects;
+				   Since YAML maps do not preserve ordering, comparing YAML as raw text will always fail.
+				   Thus compare YAML semantically and not textually.
+				*/
+				if err := yaml.Unmarshal([]byte(tc.expectedStr), &expected); err != nil {
+					t.Fatalf("failed to unmarshal expected: %v", err)
+				}
+
+				fileContent, err := os.ReadFile(tempFile.Name())
+				require.NoError(t, err, "Failed to read from temp file")
+				if err := yaml.Unmarshal(fileContent, &observed); err != nil {
+					t.Fatalf("failed to unmarshal observed: %v", err)
+				}
+
+				assert.Equal(t, expected, observed)
+				assert.Equal(t, tc.nrExpectedItemsRC, len(observed["runcmd"]))
+				assert.Equal(t, tc.nrExpectedItemsWF, len(observed["write_files"]))
+			}
+
+		})
+	}
+
+}
+
+func TestExtendUserdataRunCmd(t *testing.T) {
+	testCases := []struct {
+		name            string
+		readFileContent []byte
+		input           []string
+		expectedStr     string
+		nrExpectedItems int
+		expectedError   error
+	}{
+		{name: "case 1: add one item to section 'runcmd'",
+			readFileContent: []byte(userdataSampleContent),
+			input:           inputOneItemRunCmd,
+			expectedStr:     expectedStr2Cmd,
+			nrExpectedItems: 2,
+			expectedError:   nil,
+		},
+
+		{name: "case 2: add two items to section 'runcmd'",
+			readFileContent: []byte(userdataSampleContent),
+			input:           inputTwoItemsRunCmd,
+			expectedStr:     expectedStr3Cmd,
+			nrExpectedItems: 3,
+			expectedError:   nil,
+		},
+
+		{name: "case 3: section 'runcmd' does not exist",
+			readFileContent: []byte(userdataSampleContentNoSections),
+			input:           inputOneItemRunCmd,
+			expectedStr:     expectedStr1Cmd,
+			nrExpectedItems: 1,
+			expectedError:   nil,
+		},
+
+		{name: "case 4: no content in userdata file",
+			readFileContent: []byte{},
+			input:           inputOneItemRunCmd,
+			expectedStr:     expectedStr1Cmd,
+			nrExpectedItems: 1,
+			expectedError:   nil,
+		},
+
+		{name: "case 5: input as empty list",
+			readFileContent: []byte(userdataSampleContent),
+			input:           nil,
+			expectedStr:     userdataSampleContent,
+			nrExpectedItems: 1,
+			expectedError:   nil,
+		},
+	}
+
+	var expected, observed map[string][]any
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tempFile, err := os.CreateTemp(t.TempDir(), "userdata.yaml")
+			require.NoError(t, err, "Failed to create temp file")
+			defer func() {
+				err := tempFile.Close()
+				require.NoError(t, err, "Failed to close temp file")
+				err = os.Remove(tempFile.Name())
+				require.NoError(t, err, "Failed to delete temp file")
+			}()
+
+			if _, err := tempFile.WriteString(string(tc.readFileContent)); err != nil {
+				require.NoError(t, err, "Failed to write to temp file")
+			}
+
+			sc := NewStandardCfgManager("[]", tempFile.Name())
+
+			err = sc.ExtendUserdataRunCmd(tc.input)
+
+			if tc.expectedError != nil {
+				assert.ErrorIs(t, err, tc.expectedError,
+					fmt.Sprintf("expected: %v, but got: %v", tc.expectedError, err))
+			} else {
+
+				/* convert to YAML objects;
+				   Since YAML maps do not preserve ordering, comparing YAML as raw text will always fail.
+				   Thus compare YAML semantically and not textually.
+				*/
+				if err := yaml.Unmarshal([]byte(tc.expectedStr), &expected); err != nil {
+					t.Fatalf("failed to unmarshal expected: %v", err)
+				}
+
+				fileContent, err := os.ReadFile(tempFile.Name())
+				require.NoError(t, err, "Failed to read from temp file")
+				if err := yaml.Unmarshal(fileContent, &observed); err != nil {
+					t.Fatalf("failed to unmarshal observed: %v", err)
+				}
+
+				assert.Equal(t, expected, observed)
+				assert.Equal(t, tc.nrExpectedItems, len(observed["runcmd"]))
+			}
+
+		})
+	}
+}
+
+func TestExtendUserdataRunCmd_YamlUnmarshalingError(t *testing.T) {
+
+	testCases := []struct {
+		name             string
+		readFileContent  []byte
+		expectedErrorStr []string
+	}{
+		{name: "case 1: invalid yaml file - random ascii chars",
+			readFileContent: []byte(userdataSampleInvalidYamlContentRandomAscii),
+			expectedErrorStr: []string{
+				"yaml: unmarshal errors",
+				"line 1: cannot unmarshal !!str",
+			},
+		},
+		{name: "case 2: invalid yaml file - runcmd is not list but integer",
+			readFileContent: []byte(userdataSampleInvalidYamlContentRunCmdIsInteger),
+			expectedErrorStr: []string{
+				"module runcmd exists but is not a list",
+			},
+		},
+		{name: "case 3: invalid yaml file - runcmd is not list but string",
+			readFileContent: []byte(userdataSampleInvalidYamlContentRunCmdIsString),
+			expectedErrorStr: []string{
+				"module runcmd exists but is not a list",
+			},
+		},
+		{name: "case 4: invalid yaml file - runcmd is not list but bool",
+			readFileContent: []byte(userdataSampleInvalidYamlContentRunCmdIsBool),
+			expectedErrorStr: []string{
+				"module runcmd exists but is not a list",
+			},
+		},
+		{name: "case 5: invalid yaml file - runcmd is not list but map",
+			readFileContent: []byte(userdataSampleInvalidYamlContentRunCmdIsMap),
+			expectedErrorStr: []string{
+				"module runcmd exists but is not a list",
+			},
+		},
+		{name: "case 6: invalid yaml file - runcmd is not list but nil",
+			readFileContent: []byte(userdataSampleInvalidYamlContentRunCmdIsNil),
+			expectedErrorStr: []string{
+				"module runcmd exists but is not a list",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		t.Run(tc.name, func(t *testing.T) {
+			tempFile, err := os.CreateTemp(t.TempDir(), "userdata.yaml")
+			require.NoError(t, err, "Failed to create temp file")
+			defer func() {
+				err := tempFile.Close()
+				require.NoError(t, err, "Failed to close temp file")
+				err = os.Remove(tempFile.Name())
+				require.NoError(t, err, "Failed to delete temp file")
+			}()
+
+			if _, err := tempFile.WriteString(string(tc.readFileContent)); err != nil {
+				require.NoError(t, err, "Failed to write to temp file")
+			}
+
+			sc := NewStandardCfgManager("[]", tempFile.Name())
+			err = sc.extendUserdata(input1ItemRunCmdCast1ItemWriteFiles)
+
+			if err == nil {
+				t.Fatal("expected error but got nil")
+			} else {
+				for _, errMsg := range tc.expectedErrorStr {
+					assert.Contains(t, err.Error(), errMsg)
+				}
+			}
+
+		})
+	}
+}
+
+func TestExtendUserdataWriteFiles(t *testing.T) {
+
+	inputOneItemWriteFilesExe := []CloudConfigItem{
+		NewCloudConfigItemWriteFiles("/tmp/run.sh", "#!/bin/bash",
+			SetCustomPermissions(os.FileMode(0744)))}
+
+	inputOneItemWriteFilesSetPermissions := []CloudConfigItem{
+		NewCloudConfigItemWriteFiles("/tmp/cdi.cert", "###begin cert",
+			SetCustomPermissions(os.FileMode(0400)))}
+
+	testCases := []struct {
+		name            string
+		readFileContent []byte
+		input           []CloudConfigItem
+		expectedStr     string
+		nrExpectedItems int
+		expectedError   error
+	}{
+
+		{name: "case 1: add one item to section 'write_files'",
+			readFileContent: []byte(userdataSampleContentWriteFiles),
+			input:           inputOneItemWriteFiles,
+			expectedStr:     expectedStr2Write,
+			nrExpectedItems: 2,
+			expectedError:   nil,
+		},
+
+		{name: "case 2: add two items to section 'write_files'",
+			readFileContent: []byte(userdataSampleContentWriteFiles),
+			input:           inputTwoItemsWriteFiles,
+			expectedStr:     expectedStr3Write,
+			nrExpectedItems: 3,
+			expectedError:   nil,
+		},
+
+		{name: "case 3: section 'write_files' does not exist",
+			readFileContent: []byte(userdataSampleContentNoSections),
+			input:           inputOneItemWriteFiles,
+			expectedStr:     expectedStr1Write,
+			nrExpectedItems: 1,
+			expectedError:   nil,
+		},
+
+		{name: "case 4: input as empty list",
+			readFileContent: []byte(userdataSampleContentWriteFiles),
+			input:           []CloudConfigItem{},
+			expectedStr:     userdataSampleContentWriteFiles,
+			nrExpectedItems: 1,
+			expectedError:   nil,
+		},
+
+		{name: "case 5: add one item to section 'write_files' with executable attribute ",
+			readFileContent: []byte(userdataSampleContentWriteFiles),
+			input:           inputOneItemWriteFilesExe,
+			expectedStr:     expectedStr2WriteExe,
+			nrExpectedItems: 2,
+			expectedError:   nil,
+		},
+
+		{name: "case 6: add one item to section 'write_files' with custom permissions ",
+			readFileContent: []byte(userdataSampleContentWriteFiles),
+			input:           inputOneItemWriteFilesSetPermissions,
+			expectedStr:     expectedStr2WriteSetPermissions,
+			nrExpectedItems: 2,
+			expectedError:   nil,
+		},
+	}
+
+	var expected, observed map[string][]any
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			tempFile, err := os.CreateTemp(t.TempDir(), "userdata.yaml")
+			require.NoError(t, err, "Failed to create temp file")
+			defer func() {
+				err := tempFile.Close()
+				require.NoError(t, err, "Failed to close temp file")
+				err = os.Remove(tempFile.Name())
+				require.NoError(t, err, "Failed to delete temp file")
+			}()
+
+			if _, err := tempFile.WriteString(string(tc.readFileContent)); err != nil {
+				require.NoError(t, err, "Failed to write to temp file")
+			}
+
+			sc := NewStandardCfgManager("[]", tempFile.Name())
+
+			err = sc.ExtendUserdataWriteFiles(tc.input)
+
+			if tc.expectedError != nil {
+				assert.ErrorIs(t, err, tc.expectedError,
+					fmt.Sprintf("expected: %v, but got: %v", tc.expectedError, err))
+			} else {
+
+				/* convert to YAML objects;
+				   Since YAML maps do not preserve ordering, comparing YAML as raw text will always fail.
+				   Thus compare YAML semantically and not textually.
+				*/
+				if err := yaml.Unmarshal([]byte(tc.expectedStr), &expected); err != nil {
+					t.Fatalf("failed to unmarshal expected: %v", err)
+				}
+
+				fileContent, err := os.ReadFile(tempFile.Name())
+				require.NoError(t, err, "Failed to read from temp file")
+				if err := yaml.Unmarshal(fileContent, &observed); err != nil {
+					t.Fatalf("failed to unmarshal observed: %v", err)
+				}
+
+				assert.Equal(t, expected, observed)
+				assert.Equal(t, tc.nrExpectedItems, len(observed["write_files"]))
+			}
+
+		})
+	}
+
+}
+
+func Test_userdataFile_not_exists(t *testing.T) {
+
+	testCases := []struct {
+		name           string
+		testedFunction func() error
+	}{
+
+		{name: "case 1: method 'extendUserdata'",
+			testedFunction: func() error {
+				sc := NewStandardCfgManager("[]", "some-non-existing-file")
+				err := sc.extendUserdata([]CloudConfigItem{})
+				return err
+			},
+		},
+		{name: "case 2: method 'ExtendUserdataWriteFiles'",
+			testedFunction: func() error {
+				sc := NewStandardCfgManager("[]", "some-non-existing-file")
+				err := sc.ExtendUserdataWriteFiles([]CloudConfigItem{})
+				return err
+			},
+		},
+		{name: "case 3: method 'ExtendUserdataRunCmd'",
+			testedFunction: func() error {
+				sc := NewStandardCfgManager("[]", "some-non-existing-file")
+				err := sc.ExtendUserdataRunCmd([]string{})
+				return err
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.testedFunction()
+			if err == nil {
+				t.Fatal("expected error bot got nil")
+			} else {
+				assert.ErrorIs(t, err, fs.ErrNotExist,
+					fmt.Sprintf("expected: %v, but got: %v", fs.ErrNotExist, err))
+			}
+		})
+	}
+
 }
--- pkg/drivers/fsas/fsas.go
+++ pkg/drivers/fsas/fsas.go
@@ -645,7 +645,7 @@ func (d *Driver) innerCreate() error {
 	}
 
 	if !d.CfgManager.IsInit() {
-		cfgManager := cfgutils.NewStandardCfgManager(d.DevicesSpecJson)
+		cfgManager := cfgutils.NewStandardCfgManager(d.DevicesSpecJson, d.UserDataFile)
 		d.CfgManager = cfgManager
 	}
 
